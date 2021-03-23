85e7f485d4059bb93421e5affcffd74938b0f9f0
Tejas Deshpande <tejasdeshpande@Tejass-MacBook-Pro.local>
2021-03-23 11:42:33 +0530

    verify json parsing error

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core';
+import fs from 'fs';
 
 export type Outputs = { [index: string]: { value: string } }
 export function ParseOutputs(commandOutput: string): Outputs {
     // parse the result and save the outputs
+    fs.appendFileSync("loggingggg", "1111111111111", 'utf8')
     var result = JSON.parse(commandOutput) as { properties: { outputs: Outputs } }
+    fs.appendFileSync("loggingggg", "2222222222222", 'utf8')
     var object = result.properties.outputs
     for (const key in object) {
         if (object.hasOwnProperty(key)) {
